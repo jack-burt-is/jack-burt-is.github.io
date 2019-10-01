e8521a6a4013e942c0b4c86cf91e2eb29ad393f3
DESKTOP-A5VNKE5\jackb <jack.burt1@hotmail.co.uk>
2019-10-01 13:09:32 +0100

    Adding mail

--- php/mail.php
+++ php/mail.php
@@ -21,13 +21,13 @@ if( empty($errors))
 {
 $to = $myemail;
 $email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Email: $email_address\n Message \n $message";
+$email_body = "You have received a new message:".
+"Here are the details:\n\nName:\n$name\n\n".
+"Email:\n$email_address\n\nMessage:\n\n$message";
 $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 //redirect to the 'thank you' page
-header('Location: contact.html');
+header('Location: /contact.html');
 }
 ?>
