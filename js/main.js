d6c25ac6bf0847f759a83de578534a8c921154f0
DESKTOP-9QLJRQU\Jack <jack.burt1@hotmail.co.uk>
2019-10-07 21:11:02 +0100

    Fixed 404

--- js/main.js
+++ js/main.js
@@ -112,6 +112,22 @@ RHS.init();
 Retail.init();
 EmailConfirm.init();
 EmailFailure.init();
+FourOhFour.init();
+
+Barba.Prefetch.init();
+
+var links = document.querySelectorAll('a[href]');
+var cbk = function(e) {
+ if(e.currentTarget.href === window.location.href) {
+   e.preventDefault();
+   e.stopPropagation();
+ }
+};
+
+for(var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', cbk);
+}
+
 
 var transEffect = Barba.BaseTransition.extend({
   start: function(){
