8659386f01066a77f27615ad5e8d88fefdc09afb
DESKTOP-9QLJRQU\Jack <jack.burt1@hotmail.co.uk>
2019-10-07 18:00:05 +0100

    Reverted again

--- js/main.js
+++ js/main.js
@@ -76,68 +76,24 @@ Contact.init();
 RHS.init();
 Retail.init();
 
-ar FadeTransition = Barba.BaseTransition.extend({
-  start: function() {
-    /**
-     * This function is automatically called as soon the Transition starts
-     * this.newContainerLoading is a Promise for the loading of the new container
-     * (Barba.js also comes with an handy Promise polyfill!)
-     */
-
-    // As soon the loading is finished and the old page is faded out, let's fade the new page
-    Promise
-      .all([this.newContainerLoading, this.fadeOut()])
-      .then(this.fadeIn.bind(this));
+var transEffect = Barba.BaseTransition.extend({
+  start: function(){
+    this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
   },
-
-  fadeOut: function() {
-    /**
-     * this.oldContainer is the HTMLElement of the old Container
-     */
-
-    return $(this.oldContainer).animate({ opacity: 0 }).promise();
-  },
-
-  fadeIn: function() {
-    /**
-     * this.newContainer is the HTMLElement of the new Container
-     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-     * Please note, newContainer is available just after newContainerLoading is resolved!
-     */
-
+  fadeInNewcontent: function(nc) {
+    nc.hide();
     var _this = this;
-    var $el = $(this.newContainer);
-
-    $(this.oldContainer).hide();
-
-    $el.css({
-      visibility : 'visible',
-      opacity : 0
-    });
-
-    $el.animate({ opacity: 1 }, 400, function() {
-      /**
-       * Do not forget to call .done() as soon your transition is finished!
-       * .done() will automatically remove from the DOM the old Container
-       */
-
-      _this.done();
-    });
-  }
-});
-
-/**
- * Next step, you have to tell Barba to use the new Transition
- */
-
+    $(this.oldContainer).fadeOut(1000).promise().done(() => {
+      nc.css('visibility','visible');
+        nc.fadeIn(1000, function(){
+          _this.done();
+        })
+      });
+    }
+  });
 Barba.Pjax.getTransition = function() {
-  /**
-   * Here you can use your own logic!
-   * For example you can use different Transition based on the current page or link...
-   */
-
-  return FadeTransition;
-};
+  return transEffect;
+}
 Barba.Pjax.start();
 
 function init(labels) {
