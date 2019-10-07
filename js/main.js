3cf58a6311c78a7e481c35aa0a6327c93cc2d205
DESKTOP-9QLJRQU\Jack <jack.burt1@hotmail.co.uk>
2019-10-07 17:21:42 +0100

    Fixed RHS page + fade transiation re-added

--- js/main.js
+++ js/main.js
@@ -75,8 +75,26 @@ Projects.init();
 Contact.init();
 RHS.init();
 Retail.init();
-Barba.Pjax.start();
 
+var transEffect = Barba.BaseTransition.extend({
+  start: function(){
+    this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
+  },
+  fadeInNewcontent: function(nc) {
+    nc.hide();
+    var _this = this;
+    $(this.oldContainer).fadeOut(1000).promise().done(() => {
+      nc.css('visibility','visible');
+        nc.fadeIn(1000, function(){
+          _this.done();
+        })
+      });
+    }
+  });
+Barba.Pjax.getTransition = function() {
+  return transEffect;
+}
+Barba.Pjax.start();
 
 function init(labels) {
   if ($('html').hasClass('fp-enabled')) {
