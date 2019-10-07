8e1412c66aa4fe9a6ab35e2c0b8a3aea0f0fa5db
DESKTOP-9QLJRQU\Jack <jack.burt1@hotmail.co.uk>
2019-10-07 18:15:59 +0100

    Fixed Particles

--- js/main.js
+++ js/main.js
@@ -242,6 +242,7 @@ function retail_arrows() {
 }
 
 function index_particles() {
+
 // modified version of random-normal
 NUM_PARTICLES = 600;
 PARTICLE_SIZE = 0.5; // View heights
@@ -256,23 +257,11 @@ window.mobilecheck = function() {
     OFFSET = 5;
   }
 };
-let particles = [];
-
-
-  if (document.readystate !== 'loading') {
-    startAnimation();
-  } else {
-    document.addEventListener('DOMContentLoaded', () => {
-      startAnimation();
-    })
-  }
-
-}
-
 
 function normalPool(o){var r=0;do{var a=Math.round(normal({mean:o.mean,dev:o.dev}));if(a<o.pool.length&&a>=0)return o.pool[a];r++}while(r<100)}function randomNormal(o){if(o=Object.assign({mean:0,dev:1,pool:[]},o),Array.isArray(o.pool)&&o.pool.length>0)return normalPool(o);var r,a,n,e,l=o.mean,t=o.dev;do{r=(a=2*Math.random()-1)*a+(n=2*Math.random()-1)*n}while(r>=1);return e=a*Math.sqrt(-2*Math.log(r)/r),t*e+l}
 
 
+let particles = [];
 
 function rand(low, high) {
   return Math.random() * (high - low) + low;
@@ -372,13 +361,17 @@ function startAnimation() {
   requestAnimationFrame((time) => draw(time, canvas, ctx));
 };
 
-// // Start animation when document is loaded
-// (function () {
-//   if (document.readystate !== 'loading') {
-//     startAnimation();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', () => {
-//       startAnimation();
-//     })
-//   }
-// }());
+// Start animation when document is loaded
+(function () {
+  if (document.readystate !== 'loading') {
+    startAnimation();
+  } else {
+    document.addEventListener('DOMContentLoaded', () => {
+      startAnimation();
+    })
+  }
+}());
+
+}
+
+
